4a3541f0aa76ab2e4c0cc341d1c4d595eec95f6d
Daniel Silva <daniel1uno@gmail.com>
2024-09-11 10:47:04 -0500

    feature:new evalscripts added

--- utils/evalScripts.js
+++ utils/evalScripts.js
@@ -15,7 +15,7 @@ define(function () {
       let NDVI = index(NIR, RED)
       var validNDVIMask = 1;
 
-      if (sample.B08 + sample.B04 == 0) {
+      if (NIR + RED == 0) {
         validNDVIMask = 0;
       }
 
@@ -44,7 +44,7 @@ define(function () {
       let GNDVI = index(NIR, GREEN)
       var validGNDVIMask = 1;
 
-      if (sample.B08 + sample.B03 == 0) {
+      if (NIR + GREEN == 0) {
         validGNDVIMask = 0;
       }
 
@@ -104,19 +104,15 @@ define(function () {
     let NDMI = index(NIR, SWIR)
     var validNDMIMask = 1;
 
-    if (sample.B08 + sample.B11 == 0) {
+    if (NIR + SWIR == 0) {
       validNDMIMask = 0;
     }
 
-    var noWaterMask = 1;
-    if (sample.SCL == 6) {
-      noWaterMask = 0;
-    }
 
     return {
       data: [NDMI],
-      // Exclude nodata pixels, pixels where ndmi is not defined and water pixels from statistics:
-      dataMask: [sample.dataMask * validNDMIMask * noWaterMask],
+      // Exclude nodata pixels, pixels where ndmi is not defined
+      dataMask: [sample.dataMask * validNDMIMask],
     };
   }`,
     SAVI: `//VERSION=3
@@ -152,5 +148,115 @@ define(function () {
       dataMask: [sample.dataMask * validSAVIMask * noWaterMask],
     };
   }`,
+    NDSI: `//VERSION=3
+    function setup() {
+      return {
+        input: ["B03", "B11", "SCL", "dataMask"],
+        output: [{ id: "data", bands: 1 }, { id: "dataMask", bands: 1 }]
+      };
+    }
+
+    function evaluatePixel(sample) {
+      const GREEN = sample.B03;
+      const SWIR = sample.B11;
+
+      let NDSI = index(GREEN, SWIR)
+      var validNDSIMask = 1;
+
+      if (GREEN + SWIR == 0) {
+        validNDSIMask = 0;
+      }
+
+      var noWaterMask = 1;
+      if (sample.SCL == 6) {
+        noWaterMask = 0;
+      }
+      return {
+        data: [NDSI],
+        // Exclude nodata pixels, pixels where ndsi is not defined and water pixels from statistics:
+        dataMask: [sample.dataMask * validNDSIMask * noWaterMask],
+      };
+    }`,
+    NDWI: `//VERSION=3
+    function setup() {
+      return {
+        input: ["B03", "B08", "SCL", "dataMask"],
+        output: [{ id: "data", bands: 1 }, { id: "dataMask", bands: 1 }]
+      };
+    }
+
+    function evaluatePixel(sample) {
+      const GREEN = sample.B03;
+      const NIR = sample.B08;
+
+      let NDWI = index(GREEN, NIR)
+      var validNDWIMask = 1;
+
+      if (GREEN + NIR == 0) {
+        validNDWIMask = 0;
+      }
+
+      return {
+        data: [NDWI],
+        // Exclude nodata pixels, pixels where ndsi is not defined and water pixels from statistics:
+        dataMask: [sample.dataMask * validNDWIMask],
+      };
+    }`,
+    NDBI: `//VERSION=3
+    function setup() {
+      return {
+        input: ["B11", "B08", "SCL", "dataMask"],
+        output: [{ id: "data", bands: 1 }, { id: "dataMask", bands: 1 }]
+      };
+    }
+
+    function evaluatePixel(sample) {
+      const SWIR = sample.B11;
+      const NIR = sample.B08;
+
+      let NDBI = index(SWIR, NIR)
+      var validNDBIMask = 1;
+
+      if (SWIR + NIR == 0) {
+        validNDBIMask = 0;
+      }
+      
+      var noWaterMask = 1;
+      if (sample.SCL == 6) {
+        noWaterMask = 0;
+      }
+
+      return {
+        data: [NDBI],
+        // Exclude nodata pixels, pixels where ndsi is not defined and water pixels from statistics:
+        dataMask: [sample.dataMask * validNDBIMask * noWaterMask],
+      };
+    }`,
+    NDCI: `//VERSION=3
+    function setup() {
+      return {
+        input: ["B05", "B04", "SCL", "dataMask"],
+        output: [{ id: "data", bands: 1 }, { id: "dataMask", bands: 1 }]
+      };
+    }
+
+    function evaluatePixel(sample) {
+      const RED_EDGE = sample.B05;
+      const RED = sample.B04;
+
+      let NDCI = index(RED_EDGE, RED)
+      var validNDCIMask = 1;
+
+      if (RED_EDGE + RED == 0) {
+        validNDCIMask = 0;
+      }
+
+      return {
+        data: [NDCI],
+        // Exclude nodata pixels, pixels where ndci is not defined from statistics:
+        dataMask: [sample.dataMask * validNDCIMask],
+      };
+    }`,
+
   };
 });
